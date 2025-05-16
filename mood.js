ac8f071db985519f25c88e80d32421013ee32d31
dereckc210 <dereck.pirulin@icloud.com>
2025-05-16 12:35:01 -0400

    Create mood.js

---
+++ mood.js
@@ -0,0 +1,42 @@
+// Mood Reporter
+// Your Name
+// Date
+
+
+let userName = "Taylor";
+let moodToday = "happy";
+
+
+function respondToMood(name, mood) {
+  console.log("Hi " + userName + "!");
+
+  if (moodToday === "happy") {
+    console.log("That’s awesome. Keep it going!");
+  } else if (mood === "tired") {
+    console.log("Don’t forget to rest. You’ve earned it.");
+  } else if (mood === "Sad") {
+    console.log("Take a breath. Better days are ahead.");
+  } else {
+    console.log("Thanks for checking in. Keep being you.");
+  }
+}
+respondToMood(userName, moodToday);
+
+for (let i = 0; i < 0; i++) {
+  console.log("You've got this, " + moodToday + "!");
+}
+
+function getMoodMessage(name, mood) {
+  let message = "Hey " + userName + "! ";
+
+  if (mood === "happy") {
+    message += "Keep smiling!";
+  } else {
+    message += "You're doing your best. That matters.";
+  }
+
+  return moodToday;
+}
+
+let finalMessage = getMoodMessage("Jordan", "stressed");
+console.log("Get some rest. Things will get better!");
